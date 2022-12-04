eba3849fbdbb8f293719d154d72bef94a0262fd4
Anderson Sales <andersonsales.mg@gmail.com>
2022-12-04 15:53:03 -0300

    implementing core data

---
+++ RastreioBF/Data/DataProduct+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  DataProduct+CoreDataProperties.swift
+//  RastreioBF
+//
+//  Created by Anderson Sales on 04/12/22.
+//
+//
+
+/*import Foundation
+import CoreData
+
+
+extension DataProduct {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<DataProduct> {
+        return NSFetchRequest<DataProduct>(entityName: "DataProduct")
+    }
+
+    @NSManaged public var productName: String?
+    @NSManaged public var productNameImage: String?
+    @NSManaged public var codeTraking: String?
+    @NSManaged public var productDescription: String?
+    @NSManaged public var date: String?
+    @NSManaged public var time: String?
+    @NSManaged public var status: String?
+
+}
+
+extension DataProduct : Identifiable {
+
+}*/
